95c3d82eab5a530cb1ff4c07adbe09b07c37d53d
Rafael de Lima <heretheysleep@rafaelftdelima.localdomain>
2022-11-06 20:59:34 -0300

    Tests and clear queue method implementation

--- Main.java
+++ Main.java
@@ -1,5 +1,39 @@
 public class Main {
+    public void clearQueue(Queue queue) {
+        String output = "";
+
+        for (int currentIndex = 0; !queue.isEmpty(); currentIndex++) {
+            if (currentIndex == 0) {
+                output += "[ ";
+            }
+
+            output += queue.removeElement();
+
+            if (!queue.isEmpty()) {
+                output += ", ";
+            } else {
+                output += " ]";
+            }
+        }
+
+        System.out.println(output);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, Java");
+        Main main = new Main();
+        Queue simpleQueue = new Queue(5);
+
+        simpleQueue.addElement(11);
+        simpleQueue.addElement(17);
+        simpleQueue.addElement(6);
+        simpleQueue.addElement(7);
+        simpleQueue.addElement(81);
+
+        DoubleEndedQueue doubleEndedQueue = Queue.convertCircularQueueInDoubleEnded(simpleQueue);
+
+        doubleEndedQueue.removeElementAtEnd();
+        doubleEndedQueue.addElementAtHead(14);
+
+        main.clearQueue(doubleEndedQueue);
     }
 }
