524d9b8cdc5367c44c9e40fa2bb4ac78bd7eba61
Rafael de Lima <heretheysleep@rafaelftdelima.localdomain>
2022-11-06 20:33:50 -0300

    Transform interface in class. Implement methods and extends Queue class

--- DoubleEndedQueue.java
+++ DoubleEndedQueue.java
@@ -1,4 +1,33 @@
-public interface DoubleEndedQueue {
-    public boolean addElementAtHead(int element);
-    public int removeElementAtEnd();
+public class DoubleEndedQueue extends Queue {
+    public DoubleEndedQueue(int length) {
+        super(length);
+    }
+
+    public boolean addElementAtHead(int element) {
+        int previousElementIndex = (nextElementIndex - 1 + length) % length;
+
+        if (isFull()) {
+            return false;
+        }
+
+        elements[previousElementIndex] = element;
+        nextElementIndex = previousElementIndex;
+        numberOfElements++;
+
+        return true;
+    }
+
+    public int removeElementAtEnd() {
+        int element = Integer.MIN_VALUE;
+        int lastElementIndex = (nextElementIndex + numberOfElements - 1) % length;
+
+        if (isEmpty()) {
+            return 0;
+        }
+        
+        element = elements[lastElementIndex];
+        numberOfElements--;
+
+        return element;
+    }
 }
